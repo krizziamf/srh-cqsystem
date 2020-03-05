e4ba90e78d3f53e9f379df69f239960a3bc70c9c
krizzia <krizziafortich@gmail.com>
2020-03-05 16:21:09 +0800

    added contact footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from './component/Header';
 import Login from './component/Login';
 import View from './component/View';
 import Adult from './component/Adult';
+import Contact from './component/Contact';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
           <View />
+          <Contact />
           {/* <Login /> */}
     </div>
   );
---
+++ src/component/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style/contact.css';
+
+
+function Contact() {
+    return(
+        <footer className="contact-container">
+                <p >STO ROSARIO HOSPITAL</p>
+                <p>0932-843-6537</p>
+                <p>781-3331</p>
+        </footer>
+    )
+}
+
+export default Contact;
